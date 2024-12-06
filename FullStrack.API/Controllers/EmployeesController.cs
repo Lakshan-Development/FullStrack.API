de05b943851158a4cbf2012237470536da8837e2
Lakshan-Development <lakshansupun0624@gmail.com>
2024-12-06 11:53:25 +0530

    Add project files.

---
+++ FullStrack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,90 @@
+﻿using FullStrack.API.Database;
+using FullStrack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStrack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStrackDbContext _fullStrackDbContext;
+        public EmployeesController(FullStrackDbContext fullStrackDbContext)
+        {
+            _fullStrackDbContext = fullStrackDbContext;
+        }
+
+        [HttpGet]
+
+        public async Task <IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullStrackDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+
+            await _fullStrackDbContext.Employees.AddAsync(employeeRequest);
+            await _fullStrackDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute]Guid id)
+        {
+          var employee =  await _fullStrackDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _fullStrackDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Mobile = updateEmployeeRequest.Mobile;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _fullStrackDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStrackDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _fullStrackDbContext.Employees.Remove(employee);
+            await _fullStrackDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ FullStrack.API/Database/FullStrackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStrack.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStrack.API.Database
+{
+    public class FullStrackDbContext : DbContext
+    {
+        public FullStrackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
